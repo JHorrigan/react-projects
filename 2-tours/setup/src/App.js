1143c84247e38e00d8ef5611f8f3ff4720a91bbe
JHorrigan <jahorrigan1411@gmail.com>
2020-12-02 20:05:09 +0000

    useEffect to Call fetchTours When App Component is Rendered

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -10,9 +10,13 @@ function App() {
   const fetchTours = async () => {
     setLoading(true);
     const response = await fetch(url);
-    const tours = await fetch.json();
+    const tours = await response.json();
     console.log(tours);
-  }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
 
   if (loading) {
     return (

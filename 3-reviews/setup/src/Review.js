272830960a68b59b883cd8065a579b0f43e01710
JHorrigan <jahorrigan1411@gmail.com>
2020-12-04 18:58:29 +0000

    Display First Person Image on Basic Review Article in Review Component

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -5,8 +5,13 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
   const [index, setIndex] = useState(0);
   const {name, job, image, text} = people[index];
-  console.log(name);
-  return <h2>review component</h2>;
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+      </div>
+    </article>
+  );
 };
 
 export default Review;

b5cc6fa8e8f6bad24db649a724f62b02f4ee9c66
JHorrigan <jahorrigan1411@gmail.com>
2020-12-04 19:03:22 +0000

    Add Further Person Details to Review Article in Review Component

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -13,6 +13,10 @@ const Review = () => {
           <FaQuoteRight />
         </span>
       </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+
     </article>
   );
 };

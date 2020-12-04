83967fb7c9dfb3026ed5fda59bce81444ee724d0
JHorrigan <jahorrigan1411@gmail.com>
2020-12-04 19:16:37 +0000

    Move Surprise Me Button in Review Component

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -23,8 +23,8 @@ const Review = () => {
         <button className="next-btn">
           <FaChevronRight />
         </button>
-        <button className="random-btn">surprise me</button>
       </div>
+      <button className="random-btn">surprise me</button>
     </article>
   );
 };

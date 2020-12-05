aa3459e8a995a01f63826ad6e182301bb9a363b1
JHorrigan <jahorrigan1411@gmail.com>
2020-12-05 10:37:33 +0000

    Return Relevant Header, Div & Sections

--- 4-accordion/setup/src/App.js
+++ 4-accordion/setup/src/App.js
@@ -3,7 +3,15 @@ import data from './data';
 import SingleQuestion from './Question';
 function App() {
   const [questions, setQuestions] = useState(data);
-  return <h2>accordion project setup</h2>;
+  return(
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;

8f33d1157bcffb5dd1a2d8ce726316cdc4b1d1a8
JHorrigan <jahorrigan1411@gmail.com>
2020-12-07 19:36:03 +0000

    Use Effect to Fetch Jobs on App Load

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -5,6 +5,18 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
   const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
   return <h2>tabs project setup</h2>
 }
 

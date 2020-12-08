37639c566cecf0bbc879854d1db2ac31c270ca76
JHorrigan <jahorrigan1411@gmail.com>
2020-12-08 09:25:46 +0000

    Add Previous Button with Icon

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -25,6 +25,9 @@ function App() {
             </article>
           );
         })}
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
       </div>
     </section>
   );

d08b46db3a4de4c576cbe6c9c7d2578bafe05caf
JHorrigan <jahorrigan1411@gmail.com>
2020-12-09 18:56:03 +0000

    Add Dummy onSubmit Function to Form

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -4,10 +4,15 @@ function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('form submit');
+  }
+
   return (
     <section className="section-center">
       <h3>tired of boring lorem ipsum?</h3>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">
           paragraphs:
         </label>

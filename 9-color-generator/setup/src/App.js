1ccd746a562770c3522678955989556c71fca108
JHorrigan <jahorrigan1411@gmail.com>
2020-12-13 20:37:42 +0000

    Create SingleColor Component For Each Color In List

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -12,10 +12,9 @@ function App() {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
-      console.log(colors);
+      setList(colors);
     } catch (error) {
       setError(true);
-      console.log(error);
     }
 
   }
@@ -36,7 +35,12 @@ function App() {
       </form>
     </section>
     <section className="colors">
-      <h4>colors list</h4>
+      {list.map((color, index) => {
+        return (
+          <SingleColor key={index} {...color}
+          index={index} />
+        );
+      })}
     </section>
     </>
   );

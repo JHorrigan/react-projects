dab491aaa965e716f1926f8d6b4119efcb980eae
JHorrigan <jahorrigan1411@gmail.com>
2020-12-13 20:11:19 +0000

    Add Color Form to Set Color State

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -17,6 +17,10 @@ function App() {
     <>
     <section className="container">
       <h3>color generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={color} onChange={(e) => 
+          setColor(e.target.value)} />
+      </form>
     </section>
     <section className="colors">
       <h4>colors list</h4>

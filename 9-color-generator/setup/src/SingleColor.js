c36a4dccf7eed4d902a32fce62baeb5334279c07
JHorrigan <jahorrigan1411@gmail.com>
2020-12-13 21:09:49 +0000

    Use Effect to Clear Clipboard Message

--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -5,6 +5,14 @@ const SingleColor = ({rgb, weight, index}) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
   const hex = rgbToHex(...rgb);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000)
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
   return (
     <article className={`color ${index > 10 && 'color-light'}`} style={
       {backgroundColor:`rgb(${bcg})`}}

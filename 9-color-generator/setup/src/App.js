f8128b69fb090fe7357e6a0dae0b8ee7b221a5cf
JHorrigan <jahorrigan1411@gmail.com>
2020-12-13 20:31:30 +0000

    Try Catch & Change Input Border For Bad Color Inputs

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -10,8 +10,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let colors = new Values(color).all(10);
-    console.log(colors);
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+
   }
 
   return (
@@ -22,6 +28,7 @@ function App() {
         <input type="text" value={color} onChange={(e) => 
           setColor(e.target.value)}
           placeholder="#f15025"
+          className={`${error ? 'error' : null}`}
         />
         <button className="btn" type="submit">
           submit

786ed4021382913bf045cfbed5882bd38bbd16c8
JHorrigan <jahorrigan1411@gmail.com>
2020-12-14 09:36:16 +0000

    Set Default State Value For Alert Banner Object

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
+import { FaLessThanEqual } from 'react-icons/fa';
 
 function App() {
   const [name, setName] = useState('');
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({show:false,
+    msg="", type=""});
 
   return <h2>grocery bud setup</h2>
 }

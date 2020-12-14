acf7ed3a98ddf3c17ef106c661556649c1c52abe
JHorrigan <jahorrigan1411@gmail.com>
2020-12-14 17:56:23 +0000

    Display Alert Component Based on Alert.Show State Value

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        
+        {alert.show && <Alert />}
       </form>
       <div className="grocery-container">
         <List />

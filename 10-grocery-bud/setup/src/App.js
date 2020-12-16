37b9908297e1ae9b58ee941848057193d36d7a55
JHorrigan <jahorrigan1411@gmail.com>
2020-12-16 14:21:16 +0000

    Create Get Local Storage Function to Initialise List

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,9 +3,18 @@ import List from './List'
 import Alert from './Alert'
 import { FaLessThanEqual } from 'react-icons/fa';
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return []
+  }
+}
+
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({show:false,

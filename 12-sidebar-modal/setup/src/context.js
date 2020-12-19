6412a353f20b1107c243366bb5cd02b05b89714e
JHorrigan <jahorrigan1411@gmail.com>
2020-12-19 11:55:14 +0000

    Set Default State Value For Modal Status in App Provider

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -5,6 +5,7 @@ const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
     return <AppContext.Provider value="hello">{children}
         </AppContext.Provider>
 }

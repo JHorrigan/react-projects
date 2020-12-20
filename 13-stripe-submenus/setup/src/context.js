89ab87058325be821d8338190850b6acb1d10f0c
JHorrigan <jahorrigan1411@gmail.com>
2020-12-20 16:02:37 +0000

    Add Open Sidebar Function

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -6,6 +6,11 @@ const AppContext = React.createContext();
 const AppProvider = ({children}) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
     const [isModalOpen, setIsModalOpen] = useState(true);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+
     return (
         <AppContext.Provider>
             {children}

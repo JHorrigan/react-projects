a0cea6b44a447c846505da95806547ec9ee875ca
JHorrigan <jahorrigan1411@gmail.com>
2020-12-20 13:32:59 +0000

    Import Global Context Into Modal Component

--- 12-sidebar-modal/setup/src/Modal.js
+++ 12-sidebar-modal/setup/src/Modal.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
+import { useGlobalContext } from './context'
+
 const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalContext();
   return (
     <div className="modal-overlay">
       <div className="modal-container">

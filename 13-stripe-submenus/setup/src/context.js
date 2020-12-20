f56666f636b7e6713fa98dc23bdf07831efb6079
JHorrigan <jahorrigan1411@gmail.com>
2020-12-20 15:52:42 +0000

    Add Default State For Sidebar Open Flag

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -4,6 +4,7 @@ import sublinks from './data'
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
     return (
         <AppContext.Provider>
             {children}

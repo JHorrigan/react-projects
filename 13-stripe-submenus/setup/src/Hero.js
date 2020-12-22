45eab200811196dcf1a3c22b93bb7633280a900e
JHorrigan <jahorrigan1411@gmail.com>
2020-12-22 10:59:51 +0000

    Add Sub Paragraph Text to Info Area of Hero Component

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -12,6 +12,11 @@ const Hero = () => {
           <h1>
             Payments infrastructure for the internet           
           </h1>
+          <p>
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+          </p>
         </article>
         <article className="hero-images">
           <img src={phoneImg} className="phone-img" alt="phone" />

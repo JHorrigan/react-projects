8ed8281e524353c84bfa7a70f95715c5ed467705
JHorrigan <jahorrigan1411@gmail.com>
2020-12-22 16:22:51 +0000

    Add Use Effect When Location Changes to Update Inline CSS For Submenu

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -5,6 +5,13 @@ const Submenu = () => {
   const { isSubmenuOpen, location } = useGlobalContext();
   const container = useRef(null);
 
+  useEffect(() => {
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`
+    submenu.style.top = `${bottom}px`
+  }, [location]);
+
   return (
     <aside className={`${isSubmenuOpen ? "submenu show"
     : "submenu"}`} ref={container}>

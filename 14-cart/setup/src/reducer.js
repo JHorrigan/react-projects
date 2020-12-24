0fb8bcdb3cf411f48939129300cec4e35dcae8db
JHorrigan <jahorrigan1411@gmail.com>
2020-12-24 18:01:35 +0000

    Implement Increase Amount Type In Reducer

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -9,6 +9,15 @@ const reducer = (state, action) => {
                 action.payload)
         }
     }
+    if(action.type === 'INCREASE') {
+        let tempCart = state.cart.map((cartItem) => {
+            if(cartItem.id === action.payload) {
+                return {...cartItem, amount: cartItem.amount + 1}
+            }
+            return cartItem
+        })
+        return {...state, cart: tempCart}
+    }
     return state
 }
 

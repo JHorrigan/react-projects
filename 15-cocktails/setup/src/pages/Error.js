deb30639f420cb5e7c822ab1e896bb97da37cd92
JHorrigan <jahorrigan1411@gmail.com>
2020-12-28 12:38:02 +0000

    Add Back to Home Button to Error Page

--- 15-cocktails/setup/src/pages/Error.js
+++ 15-cocktails/setup/src/pages/Error.js
@@ -6,6 +6,9 @@ const Error = () => {
     <section className="error-page section">
       <div className="error-container">
         <h1>oops! it's a dead end</h1>
+        <Link to="/" className="btn btn-primary">
+          back home
+        </Link>
       </div>
     </section>
   )

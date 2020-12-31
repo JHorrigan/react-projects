7cc79c2ccdee47a0aafe98cab294195647118894
JHorrigan <jahorrigan1411@gmail.com>
2020-12-31 22:40:11 +0000

    Implement Search Form

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -6,9 +6,16 @@ const SearchForm = () => {
   const searchValue = React.useRef('');
 
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">
+            search your favourite cocktail
+          </label>
+          <input type="text" id="name" />
+        </div>
+      </form>
+    </section>
   )
 }
 
